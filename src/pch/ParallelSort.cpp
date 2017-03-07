40d140a7dc71f837946f11f2cce0ac707be41033
lucasjliu <jiahuan.liu@outlook.com>
2017-03-07 03:42:45 -0800

    parallel merge sort done

---
+++ src/pch/ParallelSort.cpp
@@ -0,0 +1,61 @@
+//
+//  ParallelSort.cpp
+//
+//	@brief: marginality sort of vector of points with 2 dimensionality
+//
+//	by Jiahuan.Liu
+//	jiahaun.liu@outlook.com
+//
+//  03/06/2017
+//
+
+#include <algorithm>
+#include <time.h>
+#include <functional>
+
+#include "ParallelSort.h"
+
+void testSort()
+{
+	const int size = 5e6;
+	const int ntests = 10;
+	std::vector<double> vecs[ntests], gts[ntests];
+	srand(1);
+
+	using ret_type = std::vector<double>;
+	auto paraSort = [](Timer& t, ret_type& v) {MergeSort::sort(v.begin(), v.end(), 2); return v;};
+	auto seqSort = [](Timer& t, ret_type& v) {MergeSort::sort(v.begin(), v.end(), 1); return v;};
+	auto paral = UnitTestFactory::create<ret_type>(paraSort);
+	auto seq = UnitTestFactory::create<ret_type>(seqSort);
+
+	//Timer timer;
+	//int t1, t2;
+
+	for (int t = 0; t < ntests; ++t)
+	{
+		for (int i = 0; i < size; ++i)
+		{
+			vecs[t].push_back(rand() / RAND_MAX);
+			gts[t].push_back(vecs[t][i]);
+		}
+
+		seq.addCase({}, gts[t]);
+
+		/*auto gt = gts[t];
+		timer.start();
+		std::sort(gt.begin(), gt.end());
+		t1 = timer.stop();*/
+
+		paral.addCase(gts[t], vecs[t]);
+
+		/*auto vec = vecs[t];
+		timer.start();
+		MergeSort::sort(vec.begin(), vec.end(), 1);
+		t2 = timer.stop();
+		
+		LOG_INFO << "case " << t << ": " << t1 << " " << t2;*/
+	}
+	
+	//seq.run();
+	paral.run();
+}
---
+++ src/pch/ParallelSort.h
@@ -0,0 +1,164 @@
+//
+//  ParallelSort.h
+//
+//	@brief: marginality sort of vector of points with 2 dimensionality
+//
+//	by Jiahuan.Liu
+//	jiahaun.liu@outlook.com
+//
+//  03/06/2017
+//
+
+#ifndef _PARALLEL_SORT_H
+#define _PARALLEL_SORT_H
+
+#include <vector>
+#include <iterator>
+#include <type_traits>
+#include <algorithm>
+
+#include "Logger.h"
+#include "UnitTest.h"
+
+class MergeSort;
+
+class ParallelSort
+{
+public:
+	template <typename Itr>
+	static void mergesort(Itr beg, Itr end);
+
+	template <typename Itr>
+	static void oddeven(Itr beg, Itr end);
+
+};
+
+class MergeSort
+{
+public:
+	//std::less<> slightly slower than std::sort####
+	template <typename Itr, 
+		typename Comp=std::less<typename std::iterator_traits<Itr>::value_type> >
+	static void sort(Itr beg, Itr end, int thrNum = 1, Comp comp = Comp());
+
+private:
+	template <typename Itr, typename Comp>
+	static void _merge(Itr beg, Itr mid, Itr end, 
+		typename std::vector<typename std::iterator_traits<Itr>::value_type>::iterator bufItr, 
+		Comp comp);
+
+	template <typename Itr, typename Comp>
+	static void _parallel_recursive(Itr beg, Itr end, 
+		typename std::vector<typename std::iterator_traits<Itr>::value_type>::iterator bufItr, 
+		int thrNum, Comp comp);
+
+	template <typename Itr, typename Comp>
+	static void _parallel_iterative(Itr beg, Itr end, 
+		typename std::vector<typename std::iterator_traits<Itr>::value_type>::iterator bufItr, 
+		int thrNum, Comp comp) {}
+
+	template <typename Itr, typename Comp>
+	static void _sequential(Itr beg, Itr end, Comp comp);
+
+};
+
+template <typename Itr, typename Comp>
+void MergeSort::sort(Itr beg, Itr end, int thrNum, Comp comp)
+{
+	if (thrNum <= 0) return;
+
+	if (thrNum == 1)
+	{
+		_sequential(beg, end, comp);
+		return;
+	}
+
+	const int size = end - beg;
+	
+	if (size <= 0) return;
+
+	using val_t = typename std::iterator_traits<Itr>::value_type;
+	std::vector<val_t> buf(size);
+
+	_parallel_recursive(beg, end, buf.begin(), thrNum, comp);
+}
+
+template <typename Itr, typename Comp>
+void MergeSort::_parallel_recursive(Itr beg, Itr end, 
+	typename std::vector<typename std::iterator_traits<Itr>::value_type>::iterator bufItr, 
+	int thrNum, Comp comp)
+{
+	//LOG_INFO << beg - beg << " " << end - beg << " " << thrNum;
+
+	if (thrNum <= 0) return;
+
+	if (thrNum == 1)
+	{
+		_sequential(beg, end, comp);
+		return;
+	}
+
+	const int size = end - beg;
+
+	if (size <= 0) 
+	{
+		LOG_WARN << "array size exception.";
+		return;
+	}
+
+	Itr mid = size / 2 + beg;
+
+	//if thread numbers over 1, sort in parallel
+	#pragma omp parallel sections
+	{
+		#pragma omp section
+		_parallel_recursive(beg, mid, bufItr, thrNum / 2, comp);
+		#pragma omp section
+		_parallel_recursive(mid, end, bufItr + (mid - beg), thrNum - thrNum / 2, comp);
+	}
+
+	_merge(beg, mid, end, bufItr, comp);
+}
+
+template <typename Itr, typename Comp>
+void MergeSort::_sequential(Itr beg, Itr end, Comp comp)
+{
+	std::sort(beg, end, comp);
+}
+
+template <typename Itr, typename Comp>
+void MergeSort::_merge(Itr beg, Itr mid, Itr end, 
+	typename std::vector<typename std::iterator_traits<Itr>::value_type>::iterator bufItr, 
+	Comp comp)
+{
+	using BufItr = decltype(bufItr);
+
+	const Itr beg0 = beg, end0 = mid, beg1 = mid;
+	const BufItr bufBeg = bufItr;
+
+	for (Itr itr = beg0; itr != end0; itr++, bufItr++)
+	{//copy first half to buf, so that can directly write to original space
+		(*bufItr) = (*itr);
+	}
+
+	const BufItr bufEnd = bufItr;
+	
+	//iterator for writing result, reading first half, reading second half
+	Itr itr = beg0, itr0 = bufBeg, itr1 = beg1;
+
+	while (itr0 != bufEnd)
+	{//if itr1 == end1, remaining order is correct, no need to copy
+		if (itr1 == beg1 || comp((*itr0), (*itr1)) )
+		{
+			(*itr++) = (*itr0++);
+		}
+		else
+		{
+			(*itr++) = (*itr1++);
+		}
+	}
+}
+
+void testSort();
+
+#endif //_PARALLEL_SORT_H
